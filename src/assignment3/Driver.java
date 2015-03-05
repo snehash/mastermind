e32db585491e5a07ae96d8af6b708744aaa45a12
Sneha <sneha.snstar@gmail.com>
2015-03-05 00:08:09 -0600

    Driver with main method.

---
+++ src/assignment3/Driver.java
@@ -0,0 +1,22 @@
+package assignment3;
+
+/**
+Classes to play mastermind.
+Solves EE422C programming assignment #3
+@author Sneha Shrotriya, Robert Gilmore
+@version 2.01 2015-03-06
+*/
+
+
+public class Driver {
+
+	//changing this determines whether code will be printed to console or not
+	private static final boolean DEBUG = false;
+	
+	
+    @SuppressWarnings("unused")
+	public static void main(String[] args) {
+           Gameboard Mastermind = new Gameboard(DEBUG);
+    }
+
+}
